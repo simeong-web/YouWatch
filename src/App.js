121335f8b483521f044bc4f618ea66d702c70e30
Simeon <simogeorgiew@gmail.com>
2020-04-17 14:55:35 +0300

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import HeaderNav from './containers/HeaderNav/HeaderNav';
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                Hello
+                <HeaderNav />
             </div>
-        )
+        );
     }
 };
