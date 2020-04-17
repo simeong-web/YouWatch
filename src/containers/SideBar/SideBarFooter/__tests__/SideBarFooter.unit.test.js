fd9535fc33ba3638864b2968df4bdc4f611adf67
Simeon <simogeorgiew@gmail.com>
2020-04-17 17:52:55 +0300

    Unit Testing with Jest and Enzyme

---
+++ src/containers/SideBar/SideBarFooter/__tests__/SideBarFooter.unit.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SideBarFooter from '../SideBarFooter';
+
+test('renders SideBarFooter', () => {
+    const wrapper = shallow(
+        <SideBarFooter />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+});
