7363bf1eed2d0dc47ba4809bb07e177682f28dd9
Simeon <simogeorgiew@gmail.com>
2020-04-17 16:22:11 +0300

    Side Bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import HeaderNav from './containers/HeaderNav/HeaderNav';
+import SideBar from './containers/SideBar/SideBar';
 
 export default class App extends Component {
     render() {
         return (
-            <div>
+            <>
                 <HeaderNav />
-            </div>
+                <SideBar />
+            </>
         );
     }
 };
