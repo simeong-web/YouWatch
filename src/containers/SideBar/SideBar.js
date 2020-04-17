bda40217fecdaabd0169edb3decfee5485dbb7cd
Simeon <simogeorgiew@gmail.com>
2020-04-17 17:05:52 +0300

    Finishing off Side Bar

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Divider } from 'semantic-ui-react';
 import SideBarItem from './SideBarItem/SideBarItem';
 import './SideBar.scss';
+import SideBarHeader from './SideBarHeader/SideBarHeader';
+import Subscriptions from './Subscriptions/Subscriptions';
+import SideBarFooter from './SideBarFooter/SideBarFooter';
 
 export default class SideBar extends Component {
     render() {
@@ -10,13 +13,21 @@ export default class SideBar extends Component {
                 <SideBarItem highlight={true} label='Home' icon='home' />
                 <SideBarItem label='Trending' icon='fire' />
                 <SideBarItem label='Followers' icon='spy' />
+                <Divider />
+                <SideBarHeader title='Library' />
                 <SideBarItem label='History' icon='history' />
                 <SideBarItem label='Watch later' icon='clock' />
                 <SideBarItem label='Liked videos' icon='thumbs up' />
+                <Divider />
+                <Subscriptions />
+                <SideBarHeader title='More from YouTube' />
                 <SideBarItem label='Movies and Shows' icon='film' />
+                <Divider />
                 <SideBarItem label='Report history' icon='flag' />
                 <SideBarItem label='Help' icon='help circle' />
                 <SideBarItem label='Send feedback' icon='comment' />
+                <Divider />
+                <SideBarFooter />
             </Menu>
         );
     }
---
+++ src/containers/SideBar/SideBarFooter/SideBarFooter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './SideBarFooter.scss';
+
+export default function SideBarFooter() {
+    return (
+        <>
+            <div className="footer-block">
+                <div>About Press Copyright</div>
+                <div>Creators Advertise</div>
+                <div>Developers +YouWatch</div>
+                <div>Legal</div>
+            </div>
+            <div className="footer-block">
+                <div>Terms Privacy</div>
+                <div>Policy & Safety</div>
+                <div>Test new features</div>
+            </div>
+            <div className="footer-block">
+                <div>All prices include VAT</div>
+            </div>
+            <div className="footer-block">
+                <div>&copy; Simeon Georgiev - Educational YouTube Project</div>
+            </div>
+        </>
+    )
+}
