362e884bb895694bbaa226d4e3da296fbe5c99fd
Simeon <simogeorgiew@gmail.com>
2020-04-20 14:51:26 +0300

    Added Rating and Metadata components

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './Watch.scss';
 import RelatedVideos from '../../components/RelatedVideos/RelatedVideos';
 import Video from '../../components/Video/Video';
+import VideoMetadata from '../../components/VideoMetadata/VideoMetadata';
 
 export default class Watch extends Component {
     render() {
         return (
             <div className="watch-grid">
                 <Video className="video" id='-7fuHEEmEjs' />
-                <div className="metadata" style={{ width: '100%', height: '100%', background: '#F91112' }}>Metadata</div>
+                <VideoMetadata className='metadata' viewCount={1000} />
                 <div className="video-info-box" style={{ width: '100%', height: '100px', background: '#BD10E0' }}>Video Info Box</div>
                 <div className="comments" style={{ width: '100%', height: '100px', background: '#9013FE' }}>Comments</div>
                 <RelatedVideos className="related-videos" />
