f2351171b0b93e92699142806f14a191dbe7acb5
Simeon <simogeorgiew@gmail.com>
2020-04-20 17:15:30 +0300

    Added video info box

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -3,14 +3,15 @@ import './Watch.scss';
 import RelatedVideos from '../../components/RelatedVideos/RelatedVideos';
 import Video from '../../components/Video/Video';
 import VideoMetadata from '../../components/VideoMetadata/VideoMetadata';
+import VideoInfoBox from '../../components/VideoInfoBox/VideoInfoBox';
 
 export default class Watch extends Component {
     render() {
         return (
             <div className="watch-grid">
                 <Video className="video" id='-7fuHEEmEjs' />
-                <VideoMetadata className='metadata' viewCount={1000} />
-                <div className="video-info-box" style={{ width: '100%', height: '100px', background: '#BD10E0' }}>Video Info Box</div>
+                <VideoMetadata className="metadata" viewCount={1000} />
+                <VideoInfoBox className="video-info-box" />
                 <div className="comments" style={{ width: '100%', height: '100px', background: '#9013FE' }}>Comments</div>
                 <RelatedVideos className="related-videos" />
             </div>
