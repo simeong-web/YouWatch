07b538fecb21051f44f1253c2fcaa53c2126de91
Simeon <simogeorgiew@gmail.com>
2020-04-20 17:47:53 +0300

    Added Comment Section

--- src/components/VideoInfoBox/VideoInfoBox.js
+++ src/components/VideoInfoBox/VideoInfoBox.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './VideoInfoBox.scss';
-import { Image, Button } from 'semantic-ui-react';
+import { Image, Button, Divider } from 'semantic-ui-react';
 
 export default class VideoInfoBox extends Component {
     state = { collapsed: true };
@@ -14,24 +14,27 @@ export default class VideoInfoBox extends Component {
         }
 
         return (
-            <div className='video-info-box'>
-                <Image className="channel-image" src='http://via.placeholder.com/48x48' circular />
-                <div className="video-info">
-                    <div className="channel-name">Channel Name</div>
-                    <div className="video-publication-date">Thu 24, 2017</div>
-                </div>
-                <Button color='youtube' className="subscribe">91.5K Subscribers</Button>
-                <div className="video-description">
-                    <div className={descriptionTextClass}>
-                        <p>Paragraph 1</p>
-                        <p>Paragraph 2</p>
-                        <p>Paragraph 3</p>
-                        <p>Paragraph 4</p>
-                        <p>Paragraph 5</p>
+            <>
+                <div className='video-info-box'>
+                    <Image className="channel-image" src='http://via.placeholder.com/48x48' circular />
+                    <div className="video-info">
+                        <div className="channel-name">Channel Name</div>
+                        <div className="video-publication-date">Thu 24, 2017</div>
                     </div>
-                    <Button compact onClick={this.onToggleCollapseButtonClick}>{buttonTitle}</Button>
+                    <Button color='youtube' className="subscribe">91.5K Subscribers</Button>
+                    <div className="video-description">
+                        <div className={descriptionTextClass}>
+                            <p>Paragraph 1</p>
+                            <p>Paragraph 2</p>
+                            <p>Paragraph 3</p>
+                            <p>Paragraph 4</p>
+                            <p>Paragraph 5</p>
+                        </div>
+                        <Button compact onClick={this.onToggleCollapseButtonClick}>{buttonTitle}</Button>
+                    </div>
                 </div>
-            </div>
+                <Divider />
+            </>
         );
     }
 
---
+++ src/containers/Comments/Comment/Comment.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Comment.scss';
+import { Button, Image } from 'semantic-ui-react';
+import Rating from '../../../components/Rating/Rating';
+
+export default function Comment(props) {
+    return (
+        <div className="comment">
+            <Image className="user-image" src='http://via.placeholder.com/48x48' circular />
+            <div>
+                <div className="user-name">User name</div>
+                <span>Comment text</span>
+                <div className="comment-actions">
+                    <Rating likeCount={1} />
+                    <Button size='mini' compact>Reply</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
